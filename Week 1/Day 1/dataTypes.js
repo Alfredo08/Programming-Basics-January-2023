2c28234c6ca5cb02f5f04c3452f40c08654ca669
Alfredo08 <alfredosv08@gmail.com>
2023-01-09 15:52:43 -0600

    Data Types

---
+++ Week 1/Day 1/dataTypes.js
@@ -0,0 +1,25 @@
+
+// This is an inline comments
+
+/* This
+   is
+   a
+   block
+   comment */
+
+// Data types
+var firstName = "Alex";
+var lastName = "Miller";
+var age = 25;
+var isMarried = false;
+var isGraduate; // undefined
+var siblings = null;
+
+var fullName = "Alex";
+
+console.log( firstName, lastName );
+console.log( age );
+console.log( "Is Alex married? " + isMarried );
+console.log( isGraduate );
+console.log( siblings );
+
