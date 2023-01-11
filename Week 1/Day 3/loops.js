5b3916b33b5a0c94c1aadc1e7f6616947dfd233e
Alfredo08 <alfredosv08@gmail.com>
2023-01-11 11:07:18 -0600

    Loops

---
+++ Week 1/Day 3/loops.js
@@ -0,0 +1,51 @@
+// Loop that prints Hello World 10 times using 'for'
+                          // i ++
+for( var i = 1; i <= 10; i ++ ){
+    console.log( "Hello World", i );
+}
+
+// Loop that prings Hello World 10 times using 'while'
+var j = 1;
+while( j <= 10 ){
+    console.log( "Hello World", j );
+    j = j + 1;
+}
+
+/*
+ New operators
+ ++     x = x + 1
+ --     x = x - 1        x = 5
+ +=     x = x + value    x += 10
+ -=     x = x - value
+ *=     x = x * value
+ /=     x = x / value
+
+*/
+
+/* Operators
+   () - priority operator
+    * - multiply
+    / - divide
+    % - remainder of a division (modulus)
+    + - add
+    - - subtract
+   ++ - adds 1 unit
+   -- - subtracts 1 unit
+   += - adds an amount on the right of the =
+   -= - substracts an amount on the right of the =
+   *=
+   /=
+   %=
+    = - set a value to a variable  
+*/
+/* Hierarchy of operators
+    ()
+    !
+    * / %
+    + -
+    < > <= >= === !==
+    && ||
+    ++ -- += -= *= /* %=
+    =
+
+*/
